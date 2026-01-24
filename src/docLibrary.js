28d63163aeca8ba8107d92f7082daf9edba78e9f
Mark <markssaunders@gmail.com>
2026-01-23 19:57:53 -0500

    Feature: Plumbing for UI documentation sliding drawer

---
+++ src/docLibrary.js
@@ -0,0 +1,14 @@
+const docLibrary = {
+    "pulsePressureHistogram": {
+        title: "Pulse Pressure Histogram",
+        content: `
+            <h3>What is Pulse Pressure?</h3>
+            <p>Pulse pressure is simply the difference between your systolic and diastolic readings (Sys minus Dia)...</p>
+            <ul>
+                <li>Content to be written</li>
+                <li>Content to be written</li>
+            </ul>
+            And so on.....
+        `
+    }
+};
